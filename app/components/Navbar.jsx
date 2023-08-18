fb5d40e153f82298f94cfec0d3a2b88d70cbe309
Antjrobles <antjrobles@gmail.com>
2023-08-18 10:41:53 +0200

    Client Form Component 11

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,19 +1,19 @@
-import Link  from 'next/link';
-import Image from 'next/image'
+import Link from 'next/link';
+import Image from 'next/image';
 import Logo from '../../public/AR2.svg';
-
 
 export default function Navbar() {
   return (
     <nav>
-      <Image src={Logo}
-      alt="Logo"
-      width={50}
-      quality={100}
-      />
-    <h1>Help desk</h1>
-    <Link href="/">Dashboard</Link>
-    <Link href="/tickets">Tickets</Link>
-  </nav>
-  )
+      <Image src={Logo} alt='Logo' width={50} quality={100} />
+
+      <h1>Help desk</h1>
+      <Link href='/'>Dashboard</Link>
+      <Link href='/tickets'>Tickets</Link>
+      <Link href='/tickets/create'>Add Ticket</Link>
+
+      <Link href='/login'>Login</Link>
+      <Link href='/signup'>Signup</Link>
+    </nav>
+  );
 }
