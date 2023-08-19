4f13489051293f78543b6fb773c5cc7b7dfa7b38
Antjrobles <antjrobles@gmail.com>
2023-08-19 09:41:56 +0200

    Handling POST requests 20

--- app/api/tickets/route.js
+++ app/api/tickets/route.js
@@ -1,6 +1,7 @@
 // endpoint = /api/tickets
-
 import { NextResponse } from "next/server"  // import the NextResponse object to return a response from the API
+
+export const dynamic = 'force-dynamic'  // force dynamic route
 
 export async function GET() {
   const res = await fetch('http://localhost:4000/tickets')
@@ -10,4 +11,22 @@ export async function GET() {
   return NextResponse.json(tickets, {
     status: 200
   })
+};
+
+
+export async function POST(request) {
+  const ticket = await request.json();
+
+  const res = await fetch('http://localhost:4000/tickets', {
+      method: "POST",
+      headers: {"Content-Type": "application/json"},
+      body: JSON.stringify(ticket)
+    })
+
+    const newTicket = await res.json();
+
+
+    return NextResponse.json(newTicket, {
+      status: 201
+    })
 }
