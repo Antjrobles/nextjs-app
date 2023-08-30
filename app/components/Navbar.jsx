149bef6ba069cd3d81dad4abbcde9d13b67f3369
Antjrobles <antjrobles@gmail.com>
2023-08-30 14:16:54 +0200

    protecting routes - 29

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -7,20 +7,20 @@ import LogoutButton from './LogoutButton';
 export default function Navbar({ user }) {
   return (
     <nav className='flex items-center justify-between p-4'>
-      <div className='flex items-center gap-3'>
+      <div className='flex items-center gap-3 mr-auto'>
         <Image src={Logo} alt='Logo' width={50} quality={100} />
 
-        <h1>Help desk</h1>
+        <h1>Helpdesk</h1>
         <Link href='/'>Dashboard</Link>
         <Link href='/tickets'>Tickets</Link>
         <Link href='/tickets/create'>Add Ticket</Link>
       </div>
          {user && <span className="text-primary font-bold text-xl">Hello {user.email}</span>}
          <LogoutButton />
-      <div className="flex items-center gap-3">
+      {/* <div className="flex items-center gap-3">
       <Link href='/signup'>Signup</Link>
       <Link href='/login'>Login</Link>
-      </div>
+      </div> */}
     </nav>
   );
 }
