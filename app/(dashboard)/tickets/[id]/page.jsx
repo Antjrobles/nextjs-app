896c8c97476ca5dc665a4403404099389a24aa37
Antonio Robles <antjrobles@gmail.com>
2023-08-31 07:38:21 +0200

    fetching single ticket from supabase - 34

--- app/(dashboard)/tickets/[id]/page.jsx
+++ app/(dashboard)/tickets/[id]/page.jsx
@@ -1,66 +1,53 @@
-import { notFound } from "next/navigation"; //  For the default 404
+import { notFound } from 'next/navigation'
+import { cookies } from 'next/headers'
+import { createServerComponentClient } from '@supabase/auth-helpers-nextjs'
 
-export const dynamicParams = true; // This makes the page static return a 404 if the id is not found
+export const dynamicParams = true
 
 export async function generateMetadata({ params }) {
-  const id = params.id
+  const supabase = createServerComponentClient({ cookies })
 
-  const res = await fetch(`http://localhost:4000/tickets/${id}`)
-  const ticket = await res.json()
+  const { data: ticket } = await supabase.from('tickets')
+    .select()
+    .eq('id', params.id)
+    .single()
  
   return {
-    title: `Helpdesk | ${ticket.title}`
+    title: `Helpdesk | ${ticket?.title || 'Ticket not Found'}`
   }
 }
 
+async function getTicket(id) {
+  const supabase = createServerComponentClient({ cookies })
 
-export async function generateStaticParams() {
-  const res = await fetch('http://localhost:4000/tickets');
+  const { data } = await supabase.from('tickets')
+    .select()
+    .eq('id', id)
+    .single()
 
-  const tickets = await res.json();
-
-  return tickets.map((ticket) => ({
-    id: ticket.id
-  }))
-}
-
-import Link from 'next/link';
-
-async function GetTicket(id) {
-  const res = await fetch(`http://localhost:4000/tickets/${id}`, {
-    next: {
-      revalidate: 60
+    if (!data) {
+      notFound()
     }
-  })
-
-  if(!res.ok) {
-    notFound()
-  }
-  return res.json()
+  
+    return data
 }
-
 
 export default async function TicketDetails({ params }) {
+  const ticket = await getTicket(params.id)
 
-  const ticket = await GetTicket(params.id)
   return (
     <main>
       <nav>
-        <h2>Ticket Detail</h2>
+        <h2>Ticket Details</h2>
       </nav>
       <div className="card">
         <h3>{ticket.title}</h3>
         <small>Created by {ticket.user_email}</small>
         <p>{ticket.body}</p>
         <div className={`pill ${ticket.priority}`}>
-            {ticket.priority} priority
-          </div>
+          {ticket.priority} priority
+        </div>
       </div>
-      <Link href="/tickets">
-        <h2 className="text-gray-600 hover:text-gray-950">Back to Tickets</h2>
-        </Link> 
-
-
     </main>
   )
 }
